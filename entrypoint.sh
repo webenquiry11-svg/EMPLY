3cd06d22aba32ef03a3d06cd6e93fce7f5102008
webenquiry11-svg <webenquiry11@gmail.com>
2026-08-08 13:52:10 +0530

    Run app through entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "=== 1. Running Migrations ==="
-python3 manage.py migrate --noinput
-
-echo "=== 2. Collecting Static Files ==="
-python3 manage.py collectstatic --noinput
-
-echo "=== 3. Starting Gunicorn Server ==="
+echo "=== Starting Gunicorn directly ==="
 exec gunicorn horilla.wsgi:application --bind 0.0.0.0:${PORT:-8000}
