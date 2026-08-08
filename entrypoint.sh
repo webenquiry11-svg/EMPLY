ca480fdf0a351a6551e349793059d7d4f542f275
webenquiry11-svg <webenquiry11@gmail.com>
2026-08-08 13:24:21 +0530

    Run app through entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,5 @@ python3 manage.py migrate --noinput
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
-echo "Creating initial admin user if not present..."
-python3 manage.py createhorillauser --first_name admin --last_name admin --username admin --password admin --email admin@example.com --phone 1234567890 || true
-
 echo "Starting Gunicorn..."
 exec gunicorn --bind 0.0.0.0:${PORT:-8000} horilla.wsgi:application
