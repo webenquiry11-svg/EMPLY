2c522ccfc2f4f435e8594aea3ae7ad14bca58ba9
webenquiry11-svg <webenquiry11@gmail.com>
2026-08-08 11:04:13 +0530

    Run app through entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,41 +6,21 @@
 # python3 manage.py collectstatic --noinput
 # python3 manage.py createhorillauser --first_name admin --last_name admin --username admin --password admin --email admin@example.com --phone 1234567890
 # gunicorn --bind 0.0.0.0:8000 horilla.wsgi:application
-#!/bin/sh
-# set -e
 
-# echo "PORT=$PORT"
-# echo "Waiting for database..."
+#!/bin/bash
+set -e
 
-# python3 manage.py migrate --noinput
-# python3 manage.py collectstatic --noinput
+echo "Waiting for database..."
 
-# python3 manage.py createhorillauser \
-#   --first_name admin \
-#   --last_name admin \
-#   --username admin \
-#   --password admin \
-#   --email admin@example.com \
-#   --phone 1234567890 || true
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 
-# exec gunicorn \
-#   --bind 0.0.0.0:${PORT} \
-#   horilla.wsgi:application \
-#   --access-logfile - \
-#   --error-logfile - \
-#   --log-level debug
-#!/bin/sh
-set -x
+python3 manage.py createhorillauser \
+--first_name admin \
+--last_name admin \
+--username admin \
+--password admin \
+--email admin@example.com \
+--phone 1234567890 || true
 
-echo "PORT=$PORT"
-
-python manage.py check
-
-echo "Django check finished"
-
-exec gunicorn \
-    --bind 0.0.0.0:${PORT:-10000} \
-    horilla.wsgi:application \
-    --log-level debug \
-    --access-logfile - \
-    --error-logfile -
+exec gunicorn --bind 0.0.0.0:${PORT} horilla.wsgi:application
